838e4e8927df075b5f5c54115de91543e2c119b2
Sugansdv <suganyasdv16@gmail.com>
2025-06-17 09:24:52 +0530

    added new folder

--- Day_22/src/components/Task1.jsx
+++ Day_22/src/components/Task1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Task1() {
   return (
     <div className='text-center mt-5'>
-      <h4>Task 1: Setup a Redux slice with initial states: in taskSlice.js</h4>
+      <h4>Task 1: Setup a Redux slice with initial states: in Reducer.js</h4>
     </div>
   )
 }
